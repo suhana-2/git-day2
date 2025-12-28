7e194df50531c41101dea8a99fb4a7ec39e81690
Suhana <suha2730@gmail.com>
2025-12-28 21:45:34 +0530

    Day3: Added conditions and while loop

---
+++ day3.py
@@ -0,0 +1,34 @@
+age=18
+if age>=18:
+    print("you are eligible to vote")
+
+number=5
+if number%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+marks=75
+if marks>=90:
+    print("Grade A")
+elif marks>=60:
+    print("Grade B")
+else:
+    print("Grade C")
+
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+numbers=[1,2,3,4,5,6]
+for x in numbers:
+    if x%2==0:
+        print(x,"is even")
+
+i = 1
+
+while i <= 10:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
