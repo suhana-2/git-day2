7d63a86e0f7c9ce94fdceead0921e0db5b7a3e1d
Suhana <suha2730@gmail.com>
2025-12-31 21:09:01 +0530

    Day 4: Understood basics of functions

---
+++ day4.py
@@ -0,0 +1,15 @@
+def greet():
+    print("welcome to world")
+greet()
+
+def greet(name):
+    print("hello",name)
+greet("suhana")
+greet("world")
+
+def add(a,b):
+    return a+b
+result=add(3,5)
+print(result)
+
+print("hello")
