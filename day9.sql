be0813346f405bc1827ce539064fdafd93d08ba8
Suhana <suha2730@gmail.com>
2026-01-12 14:58:58 +0530

    Day9: SQL constraints+python recall

---
+++ day9.sql
@@ -0,0 +1,4 @@
+SELECT title, director FROM movies WHERE title LIKE "Toy Story%";
+SELECT title, director FROM movies WHERE director = "John Lasseter";
+SELECT title, director FROM movies WHERE director != "John Lasseter";
+SELECT * FROM movies WHERE title LIKE "WALL-_";
